0446b9781c2eab33a7c59e55a6b62abdb6e4baf0
ArielSurco <arielsurcoa@gmail.com>
2023-10-18 18:38:17 -0300

    feat: add pill component and its story

---
+++ src/components/Pill/Pill.tsx
@@ -0,0 +1,46 @@
+import { Badge } from 'react-bootstrap'
+
+import { concat } from '../../utils/functionUtils'
+import { PillState, PillVariant } from './PillTypes'
+import { renderBadges } from './PillUtils'
+
+import './Pill.scss'
+
+export interface PillProps {
+  label: string
+  className?: string
+  bgVariant?: PillVariant
+  textVariant?: PillVariant
+  state?: PillState | PillState[]
+  width?: number | string
+  height?: number | string
+}
+
+export const Pill = ({
+  label,
+  className,
+  bgVariant,
+  textVariant,
+  width,
+  height,
+  state,
+}: PillProps) => {
+  return (
+    <div className={concat('pill-container', className)}>
+      <Badge
+        pill
+        className='pill'
+        bg={bgVariant}
+        style={{
+          width: width,
+          height: height,
+        }}
+        text={textVariant}
+      >
+        {label}
+      </Badge>
+
+      {renderBadges(state)}
+    </div>
+  )
+}
