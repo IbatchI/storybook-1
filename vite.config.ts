0a83345a307f29e0ced5c900882b739143e9d48e
ArielSurco <arielsurcoa@gmail.com>
2023-11-08 17:07:22 -0300

    fix: testing vite library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts(), cssInjectedByJsPlugin()],
   css: {
     preprocessorOptions: {
       scss: {
@@ -12,6 +15,21 @@ export default defineConfig({
           @import "./src/styles/_bsOverride.scss";
           @import "./src/styles/_variables.scss";
         `,
+      },
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'Carousel',
+      fileName: 'react-carousel',
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
       },
     },
   },
